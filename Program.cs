bb06e028bc9c7c55102a48ad8da2fa217060a99d
zenin5479 <zenin5479@gmail.com>
2024-05-24 23:42:52 +0300

    Update 0.3 Обновлен метод

--- Program.cs
+++ Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Net.Sockets;
 using System.Text;
 using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Threading;
 
 namespace DictionarySystemTextJson
@@ -11,12 +13,13 @@ namespace DictionarySystemTextJson
    {
       static void Main()
       {
-         // Десериализация словарей System.Text.Json
+         // Десериализация словарей System.Text.Json по строке
          string json = @"{""User"":""YoRHa2B"",""key"":""123""}";
          Dictionary<string, string> values = JsonSerializer.Deserialize<Dictionary<string, string>>(json);
          Console.WriteLine(values.Count);
          Console.WriteLine(values["User"]);
 
+         // Десериализация словарей System.Text.Json по классу
          MyClient element = new MyClient
          {
             Data = new StringBuilder(@"{""User"":""YoRHa2B"",""key"":""123""}")
@@ -31,7 +34,8 @@ namespace DictionarySystemTextJson
          {
             Console.WriteLine($"{keyvaluepair.Key} = {keyvaluepair.Value}");
          }
-
+         
+         
          string json2 = @"{""ABC"": {""Name"": ""Bob"", ""Age"": ""40""},""DEF"": {""Type"": ""Cat"",""Sound"": ""Meow""}}";
          var dictionary = JsonSerializer.Deserialize<Dictionary<string, string>>(json2);
 
@@ -40,6 +44,14 @@ namespace DictionarySystemTextJson
             Console.WriteLine($"{key}={val}");
          }
 
+         // Десериализация словарей System.Text.Json по строке из файла
+         string jsonString = File.ReadAllText("YoRHa2B.json");
+
+         //Dictionary<string, string> valuesclient2 = JsonConvert.DeserializeObject<Dictionary<string, string>>(jsonString);
+         //foreach (KeyValuePair<string, string> keyvaluepair in valuesclient2)
+         //{
+         //   Console.WriteLine($"{keyvaluepair.Key} = {keyvaluepair.Value}");
+         //}
 
          Console.ReadKey();
       }
