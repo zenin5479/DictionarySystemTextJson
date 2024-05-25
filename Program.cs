1a01fef957409d4b055948e059d4ea9cd33e96c1
zenin5479 <zenin5479@gmail.com>
2024-05-25 19:56:34 +0300

    Update 1.5 Обновлен метод

--- Program.cs
+++ Program.cs
@@ -1,10 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Net.Sockets;
 using System.Text;
 using System.Text.Json;
-using System.Threading;
 
 namespace DictionarySystemTextJson
 {
